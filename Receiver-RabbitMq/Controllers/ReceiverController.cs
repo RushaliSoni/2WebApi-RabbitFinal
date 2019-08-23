a15b5a7e47f0bf36c8bc8be7a36281dfe381c7ca
akshansha <akshanshagohil1995@gmail.com>
2019-08-23 13:23:29 +0530

    comment

--- Receiver-RabbitMq/Controllers/ReceiverController.cs
+++ Receiver-RabbitMq/Controllers/ReceiverController.cs
@@ -40,12 +40,7 @@ namespace Receiver_RabbitMq.Controllers
                                      arguments: null);
 
                 var consumer = new EventingBasicConsumer(channel);
-                //consumer.Received += (model, ea) =>
-                //{
-                //    var body = ea.Body;
-                //    var message = Encoding.UTF8.GetString(body);
-                //    //Console.WriteLine(" [x] Received {0}", message);
-                //};
+                
                 channel.BasicConsume(queue: "Rushalisoni",
                                      autoAck: true,
                                      consumer: consumer);
@@ -57,7 +52,7 @@ namespace Receiver_RabbitMq.Controllers
                 };
 
                 channel.QueuePurge("Rushalisoni");
-
+                //return message send from sender side 
                 return Ok(message);
 
                 
